a8dd73b8514a0240cabe7429598ddd5d2c380ffe
Anuved Verma <anuvedverma@uchicago.edu>
2016-11-08 07:08:53 -0600

    run script

--- run.sh
+++ run.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
-# example of the run script for running the fraud detection algorithm with a python file,
-# but could be replaced with similar files from any major language
-
 # I'll execute my programs, with the input directory paymo_input and output the files in the directory paymo_output
-#python ./src/antifraud.py ./paymo_input/batch_payment.txt ./paymo_input/stream_payment.txt ./paymo_output/output1.txt ./paymo_output/output2.txt ./paymo_output/output3.txt
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo $DIR
+# python ./src/antifraud.py ./paymo_input/batch_payment.txt ./paymo_input/stream_payment.txt ./paymo_output/output1.txt ./paymo_output/output2.txt ./paymo_output/output3.txt
 
-# SUN JAVAC BUG: enum class Feature must be compiled first, otherwise we get incorrect errors
+# SUN JAVAC BUG: enum class Feature must be compiled first, otherwise we get errors
 javac -classpath ./src/Feature.java ./src/*.java
-cd src
-java PaymoDriver $DIR/paymo_input/batch_payment.csv $DIR/paymo_input/stream_payment.csv $DIR/paymo_output/output1.txt $DIR/paymo_output/output2.txt $DIR/paymo_output/output3.txt
+
+# Unable to find main class and pass in arguments with current directory structure
+java PaymoDriver ./paymo_input/batch_payment.csv ./paymo_input/stream_payment.csv ./paymo_output/output1.txt ./paymo_output/output2.txt ./paymo_output/output3.txt
