be061bee71f0cb3f05e3e16eace017ef5fa376ae
Anuved Verma <anuvedverma@uchicago.edu>
2016-11-08 07:20:07 -0600

    bash message

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # I'll execute my programs, with the input directory paymo_input and output the files in the directory paymo_output
 # python ./src/antifraud.py ./paymo_input/batch_payment.txt ./paymo_input/stream_payment.txt ./paymo_output/output1.txt ./paymo_output/output2.txt ./paymo_output/output3.txt
 
-# SUN JAVAC BUG: enum class Feature must be compiled first, otherwise we get errors
+# UNRESOLVED SUN JAVAC BUG: ENUM CLASSES MUST BE COMPILED FIRST, OTHERWISE JAVAC THROWS INCORRECT ERRORS
 javac -classpath ./src/Feature.java ./src/*.java
 
 # Unable to find main class and pass in arguments with current directory structure
