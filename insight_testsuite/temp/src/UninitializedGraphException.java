8c92668db7d7fc974eee911da25de2258ef2b549
Anuved Verma <anuvedverma@uchicago.edu>
2016-11-10 01:55:23 -0600

    stable submission v1.0

--- insight_testsuite/temp/src/UninitializedGraphException.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Anuved on 11/5/2016.
- *
- * @ClassException UninitializedGraphException
- * Exception thrown if Paymo stream payment analysis attempted before graph is initialized with batch payments.
- */
-public class UninitializedGraphException extends Exception {
-
-}
